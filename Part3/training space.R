98674cfd48eea171ea5598f4c8aba3f72f5e6598
limwonki0619 <limwk0619@naver.com>
2019-05-30 17:37:39 +0900

    training

--- Part3/training space.R
+++ Part3/training space.R
@@ -19,12 +19,12 @@ names(testfile)
 colnames(testfile)[7] <- "등급"
 
 testfile2 <- rbind(testfile, data.frame(순위=30,
-                                          이름="김준호",
-                                          조=3,
-                                          점수=45,
-                                          환산점수=57.8,
-                                          성취율=0.50,
-                                          등급="A")); testfile2
+                                        이름="김준호",
+                                        조=3,
+                                        점수=45,
+                                        환산점수=57.8,
+                                        성취율=0.50,
+                                        등급="A")); testfile2
 
 subset(testfile, 점수<45 | 성취율>0.80)
 subset(testfile, 조!=3 & 환산점수 < 75)
