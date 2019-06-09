420de629a75335523bc35d62594d903e4a629d43
limwonki0619 <limwk0619@naver.com>
2019-06-09 23:41:55 +0900

    이상치 제거 후 그래프 그려보기

--- Part4/낙서장.R
+++ Part4/낙서장.R
@@ -7,6 +7,9 @@ boxplot(iris$Sepal.Length~iris$Species)
 #          Q3+1.5*IQR 보다 큰 값
 # IQR(Inter-Quartile Range)
 # 참고 : https://m.blog.naver.com/PostView.nhn?blogId=2000051148&logNo=221157875274&proxyReferer=https%3A%2F%2Fwww.google.com%2F
+str(iris)
+summary(iris)
+
 
 setosa <- iris %>% filter(Species=="setosa") %>% select(-Species)
 boxplot(setosa)$stats
@@ -14,4 +17,23 @@ for (i in 1:4) {
   setosa[,i] <- ifelse(setosa[,i] < boxplot(setosa[,i])$stats[1] | setosa[,i] > boxplot(setosa[,i])$stats[5], NA, setosa[,i])
 }
 boxplot(setosa, na.rm=T, main = "이상치 제거 setosa")
+
+
+versicolor <- iris %>% filter(Species=="versicolor") %>% select(-Species)
+boxplot(versicolor)$stats
+for (i in 1:4) {
+  versicolor[,i] <- ifelse(versicolor[,i] < boxplot(versicolor[,i])$stats[1] | versicolor[,i] > boxplot(versicolor[,i])$stats[5], NA, versicolor[,i])
+}
+boxplot(versicolor, na.rm=T, main = "이상치 제거 versicolor")
+
+
+virginica <- iris %>% filter(Species=="virginica") %>% select(-Species)
+boxplot(virginica)$stats
+for (i in 1:4) {
+  virginica[,i] <- ifelse(virginica[,i] < boxplot(virginica[,i])$stats[1] | virginica[,i] > boxplot(virginica[,i])$stats[5], NA, virginica[,i])
+}
+boxplot(virginica, na.rm=T, main = "이상치 제거 virginica")
+
+par(mfrow=c(1,3))
+
 
