ac14c7f833ad1930e92cfbde9fc08dd73a906fba
limwonki0619 <limwk0619@naver.com>
2019-06-10 08:28:30 +0900

    .

---
+++ Part4/sec 2 (ggplot2).R
@@ -0,0 +1,10 @@
+getwd()
+setwd("D:/limworkspace/R_lecture/Part4")
+setwd("D:/limworkspace/R_lecture/Part4/data")
+setwd("D:/limworkspace/R_lecture/Part4/graph")
+getwd()
+
+#--------------------------------------------------------------#
+#------------------------- Part 4 시각화 ----------------------#
+#-------------------- Section 2 : ggplot2  --------------------#
+#--------------------------------------------------------------#
