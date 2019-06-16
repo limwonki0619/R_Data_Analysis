7842a7282d2c95e451d6462147d24853675ba7cf
Wonki Lim <51067315+limwonki0619@users.noreply.github.com>
2019-06-17 08:55:31 +0900

    factor ordering

--- Part4/sec 2 (ggplot2).R
+++ Part4/sec 2 (ggplot2).R
@@ -123,3 +123,38 @@ arrange(score_kem, 이름, 과목) %>%
   group_by(이름) %>%
   summarise(누적합계 = sum(점수))
 
+
+
+# factor로 자동정렬 제어하기 *** ------------------------------------------------------------------------------------------------------------ 
+
+setwd("D:/limworkspace/R_Data_Analysis/Part2/Stage3_StructuredData/challanging/3.7 마포")
+getwd()
+
+# library(extrafont)
+
+library(dplyr)
+library(ggplot2)
+
+# windowsFonts(hanna=windowsFont("BM HANNA 11yrs old")) # 폰트이름 변경 
+# windowsFonts(dohyeon=windowsFont("BM DoHyeon"))
+# windowsFonts(jalnan=windowsFont("Jalnan"))
+# windowsFonts(binggrae=windowsFont("Binggrae Taom"))
+
+library(reshape2)
+
+mapo <- read.csv("data/마포09번이용현황.csv")
+mapo$정류소명 <- paste0(1:length(mapo$정류소명),". ",mapo$정류소명)
+mapo$정류소명 <- factor(mapo$정류소명, levels=unique(mapo$정류소명)) # 버스정류장 순서정하기 *** 
+
+mapo %>% 
+  melt(id=c("정류소명"),variable.name = "승ㆍ하차인원") %>% 
+    ggplot(aes(x=정류소명, y=value/10000,fill=승ㆍ하차인원)) +
+    geom_bar(stat="identity", position = "stack") +
+    geom_hline(yintercept = seq(0,8,2), linetype=2,color="grey10") +
+      theme_bw(base_family = "jalnan", base_size = 20) +
+      labs(title = "마포09번 마을버스 이용 현황 분석", y="승ㆍ하차인원(만 명)") +
+      theme(plot.title = element_text(family = "jalnan", face="bold", size=15, color="grey20"),
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(angle = 90),
+        legend.position = "top")
+
