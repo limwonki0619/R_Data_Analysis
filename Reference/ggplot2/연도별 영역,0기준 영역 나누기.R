0abd54915613d5791be13de548d1aa1f94ea0b16
limwonki0619 <limwk0619@naver.com>
2019-06-24 08:53:06 +0900

    value값 수정

--- Reference/ggplot2/연도별 영역,0기준 영역 나누기.R
+++ Reference/ggplot2/연도별 영역,0기준 영역 나누기.R
@@ -4,8 +4,7 @@ library(ggplot2)
 library(reshape)
 
 TS <- data.frame(date = seq(as.Date("2012-01-01"), as.Date("2019-01-01"), by="month"),
-                 value = runif(85, 0, .5),
-                 value2 = runif(85, -.5, .5))
+                 value = c(runif(30, -.3, 0), runif(55,0,0.3)))
 
 # 연도별로 영억 채우기 
 ggplot(TS,aes(x=date,y=value)) +
@@ -17,21 +16,23 @@ ggplot(TS,aes(x=date,y=value)) +
   geom_area(data = TS[grep("[^2012|2013|2014|2015]",substr(TS$date,1,4)),], fill="cyan1", alpha=0.3)
 
 # 0을 기준으로 위아래 다른색 채우기 
-ggplot(TS, aes(x=date, y=value2)) +
-  geom_col(data = TS[TS$value2 <= 0,], fill = "Coral1", alpha = .3) +
-  geom_col(data = TS[TS$value2 > 0,], fill = "royalblue1", alpha = .3) +
+# 막대
+ggplot(TS, aes(x=date, y=value)) +
+  geom_col(data = TS[TS$value <= 0,], fill = "Coral1", alpha = .3) +
+  geom_col(data = TS[TS$value > 0,], fill = "royalblue1", alpha = .3) +
   geom_hline(yintercept = 0, color="black") +
   theme_bw()
 
-TS %>% select(date,value2) %>% 
+# 시계열 
+TS %>% select(date,value) %>% 
   melt(id=c("date")) %>%
   mutate(area = ifelse(value > 0,"up","down")) %>%
   ggplot(aes(x=date, y=value)) + 
-  geom_area(aes(fill=area)) +
+  geom_area(aes(fill=area), alpha=0.3) +
   geom_line() +
   geom_hline(yintercept = 0, color='black')
 
-ggplot(TS, aes(x = date, y = value2)) +
-  geom_area(data = subset(TS, value2 <= 0), fill = "Coral1", alpha=0.3) +
-  geom_area(data = subset(TS, value2 >= 0), fill = "cyan", alpha=0.3) +
+ggplot(TS, aes(x = date, y = value)) +
+  geom_area(data = subset(TS, value <= 0), fill = "Coral1", alpha=0.3) +
+  geom_area(data = subset(TS, value >= 0), fill = "cyan", alpha=0.3) +
   theme_bw()
